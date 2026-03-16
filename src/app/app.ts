c11ff669d435afa1db85d8be144eae3377401e3a
hseingh94 <hsein.gh.94@hotmail.com>
2026-03-16 12:32:30 +0200

    Update RSVP to minimalist modal and add guest name inputs

--- src/app/app.ts
+++ src/app/app.ts
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit, OnDestroy {
   guestNames: string[] = ['']; 
 
   slides = [
-    '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg',
+    '1.jpg',  '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg',
     '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg'
   ];
   
